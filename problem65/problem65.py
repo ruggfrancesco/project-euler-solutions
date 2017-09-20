dc687a90fb2d0bba7107ccd550f9d5a9ec9e1450
Francesco Ruggeri <franci@franci>
2017-09-20 16:09:34 +0200

    Added problem 65

--- problem65/problem65.py
+++ problem65/problem65.py
@@ -1,14 +1,7 @@
-def search(mx):
-	a,b = 0,1
-	for i in range(mx):
-		a=a+b
-		b=b+a
-		a,b = b,a
-		yield(a,b)
-		
-if __name__ == '__main__':
-	s = search(10)
-	print(list(s))
+d,n = 1,2
+	
+for i in range(2,100):
+	c = 2 * i / 3 if(not i % 3) else 1
+	d, n = n, c*n + d
 
-
-
+print("Result:", sum([int(dgt) for dgt in str(int(n*10))]))
