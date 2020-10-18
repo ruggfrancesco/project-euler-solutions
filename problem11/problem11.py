3438048fdf6c7e6b943676b11e70967e3aae60e9
Francesco Ruggeri <ruggfrancesco@gmail.com>
2020-10-18 14:57:33 +0200

    problem 11

--- problem11/problem11.py
+++ problem11/problem11.py
@@ -8,17 +8,19 @@ prod = lambda a,b: a*b
 
 for y in range(len(grid)-treshold):
 	for x in range(len(grid[y])-treshold):
-		highest = max(highest, \
-									*[reduce(prod, slc) \
-									for slc in [
-										 *[grid[y][x:x+3]],
-											[grid[i][x] for i in range(y, y+4)],
-											[grid[y+i][x+i] for i in range(4)],
-											[grid[y+i][x-(i+1)] for i in range(4)]
-										]
-									])
-
 		'''
+		highest = max(highest, \
+					*[reduce(prod, slc) \
+					for slc in [
+						 *[grid[y][x:x+3]],
+							[grid[i][x] for i in range(y, y+4)],
+							[grid[y+i][x+i] for i in range(4)],
+							[grid[y+i][x-(i+1)] for i in range(4)]
+						]
+					])
+		'''
+
+		
 		# A more readable (and almost equal) solution
 		highest = max(highest,
 									*[reduce(prod, *[grid[y][x:x+3]]),
@@ -26,6 +28,6 @@ for y in range(len(grid)-treshold):
 										reduce(prod, [grid[y+i][x+i] for i in range(4)]),
 										reduce(prod, [grid[y+i][x-(i+1)] for i in range(4)])
 								])
-		'''
+		
 
 print('Result:', highest)
